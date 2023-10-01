ba2386891e52aced069237ef0591b10e37421859
oat431 <oat431@gmail.com>
2023-10-01 23:14:15 +0700

    🔨 add: connect to gateway

--- src/main/java/panomete/playground/demoservice/controller/DemoController.java
+++ src/main/java/panomete/playground/demoservice/controller/DemoController.java
@@ -2,10 +2,12 @@ package panomete.playground.demoservice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/demo")
 public class DemoController {
 
     @GetMapping("/hello")
