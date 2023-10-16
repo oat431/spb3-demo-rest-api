017173a747e42a80ecba49003c261d703a9b1f12
oat431 <oat431@gmail.com>
2023-10-16 22:06:43 +0700

    🔧 update: register to eureka server

--- src/main/java/panomete/playground/demoservice/DemoserviceApplication.java
+++ src/main/java/panomete/playground/demoservice/DemoserviceApplication.java
@@ -2,8 +2,10 @@ package panomete.playground.demoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoserviceApplication {
 
     public static void main(String[] args) {
